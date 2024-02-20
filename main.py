927751e72cceb8758fd0b1c225c3e314e9b92918
Jonty <jontybylos@gmail.com>
2024-02-20 13:34:48 -0800

    Added functionality to count number of characters in a text

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     
-    words = file_contents.split()
+    
+    #Add dictionary where lower-case letter : count
+    def let_count():
+        words = file_contents.lower().split()
+        letter_count = {}
 
-    word_count = len(words)
-    print("Word count: ", word_count)
+        for word in words:
+            for letter in word:
+            
+                letter_count[letter] = letter_count.get(letter, 0) + 1
+    
+        print(letter_count)
+    
+    let_count()
 
 main()
 
