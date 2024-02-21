741a28f82a7fd68180558e529ba78bffc56ea10d
Jonty <jontybylos@gmail.com>
2024-02-20 21:44:11 -0800

    Added functionality to order list the count of unique characters, and print a report

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_path(book_path)
-    print("Number of words:", number_of_words(text))
-    print("Unique characters and their count: \n", let_count(text))
+    
+    #print(sort_char(char_count(text)))
+    print(print_report(book_path, number_of_words(text), sort_char(char_count(text))))
 
 
 def get_book_path(path):
@@ -15,17 +16,42 @@ def number_of_words(text):
     return len(words)
 
 
-#Add dictionary where lower-case letter : count
-def let_count(text):
+def char_count(text):
     words = text.lower().split()
     letter_count = {}
 
     for word in words:
         for letter in word:
-        
             letter_count[letter] = letter_count.get(letter, 0) + 1
-
     return letter_count
+
+#Convert dictionary of characters and their count into a sorted list
+def sort_list(dict):
+    return dict["num"]
+
+def sort_char(dict):
+    char_sort = []
+    for char in dict:
+        char_count = dict[char]
+        char_sort.append({"char": char, "num": char_count})
+        
+    for dict in char_sort:
+        char_sort.sort(reverse=True, key=sort_list)
+        
+    return char_sort
+    
+#Aggregate all the word and character data into a report that we can print to the console
+def print_report(path, number_of_words, ordered_list):
+    
+    print(
+        f"--- Begin report of {path} ---\n"
+        f"{number_of_words} words found in the document\n\n"
+    )
+    for item in ordered_list:
+        print(f"The {item['char']} character was found {item['num']} times\n")
+    
+    return f"Print report for {path} Completed"
+    
     
 
 main()
