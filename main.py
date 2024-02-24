ab934c4e77b05bcd65f36fc4fb09fdd9031794b8
Jonty <jontybylos@gmail.com>
2024-02-24 11:30:47 -0800

    Fixed formatting and readability

--- main.py
+++ main.py
@@ -44,8 +44,12 @@ def print_report(path, number_of_words, ordered_list):
     for item in ordered_list:
         print(f"The {item['char']} character was found {item['num']} times\n")
     
-    return f"Print report for {path} Completed"
+
     
+    return f"Print report for {path} Completed"
+
+
+
 main()
 
 
